ddfd7e69eafeffe4e900aba6303ded85c5bfa686
Luke <luke.jewers@gmail.com>
2021-05-14 12:35:40 +0100

    updated

--- src/components/Function.tsx
+++ src/components/Function.tsx
@@ -3,28 +3,59 @@ import styled from "styled-components";
 
 const Function: React.FC<any> = ({ f }) => {
   return (
-    <Wrapper>
+    <Container>
       <h1>{f.name}</h1>
       <Type>
-        {f.name} :: {f.type}
+        <Bold>{f.name}</Bold> : : {f.type}
       </Type>
-      <div className=''>{f.function}</div>
-    </Wrapper>
+      <Description>{f.description}</Description>
+      <div>
+        <Bold>
+          <p>{f.function}</p>
+          <p>{f.alternative}</p>
+        </Bold>
+      </div>
+      <GreyDiv>
+        <p>
+          {">"} {f.application}
+        </p>
+        <p>{f.return}</p>
+      </GreyDiv>
+      {f.link ? <div className=''>See also {f.link}</div> : ""}
+    </Container>
   );
 };
 
-const Wrapper = styled.div`
+const Container = styled.div`
   display: block;
   padding: 1rem;
   margin: 1rem;
   box-shadow: 0 0 0 1px #ccc;
+  & > *:not(:last-child) {
+    margin-bottom: 0.75rem;
+  }
 `;
 
 const Type = styled.div`
+  background-color: #f2f2f2;
+  border-bottom: 3px solid #e5e5e5;
+  padding: 0.5rem 1rem;
+`;
+
+const Bold = styled.span`
+  font-weight: 700;
+`;
+
+const GreyDiv = styled.div`
   background-color: #f7f7f7;
   border: 1px solid #ddd;
   border-radius: 0.125rem;
   padding: 0.5rem 1rem;
+`;
+
+const Description = styled.p`
+  padding-left: 1rem;
+  border-left: 1px solid gainsboro;
 `;
 
 export default Function;
