a3a724fe9b0b534e6057523aa040fd3147003948
Luke <luke.jewers@gmail.com>
2021-06-04 17:12:10 +0100

    updated

--- src/lib/drop.test.tsx
+++ src/lib/drop.test.tsx
@@ -1,7 +1,7 @@
 import { drop } from "./drop";
 
 describe("drop", () => {
-  it("returns an array with n elements dropped from the beginning", () => {
+  it("should return an array with n elements dropped from the beginning", () => {
     expect(drop(2)([1, 2, 3, 4, 5])).toEqual([3, 4, 5]);
   });
 });
